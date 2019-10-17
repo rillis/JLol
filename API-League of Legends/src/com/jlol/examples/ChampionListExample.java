c1fc098dc16ec8335c5022f2b2e5ebd8e1b26be6
rillis <rillisnelson@gmail.com>
2019-10-17 00:57:29 -0300

    example locale fix

--- API-League of Legends/src/com/jlol/examples/ChampionListExample.java
+++ API-League of Legends/src/com/jlol/examples/ChampionListExample.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import com.jlol.LolAPI;
 import com.jlol.champion.Champion;
 import com.jlol.champion.ChampionList;
+import com.jlol.locale.Locale;
 
 class ChampionListExample {
 	public static void main(String[] args) {
@@ -14,7 +15,7 @@ class ChampionListExample {
 		String last_version = l.getLastVersion();
 		
 		try {
-			Champion[] c = new ChampionList(l).getChampionList(last_version, "en_US");
+			Champion[] c = new ChampionList(l).getChampionList(last_version, Locale.PORTUGUESE);
 			
 			for (int i = 0; i < c.length; i++) {
 				System.out.println(c[i].name+", "+c[i].title);
